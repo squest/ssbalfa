c5a6f66a7ba52860b34254094b3a7774b7b55ef4
squest <sabdaps@gmail.com>
2015-04-06 21:24:43 +0700

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject ssbone "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
+                 [compojure "1.3.3"]
+                 [ring/ring-defaults "0.1.4"]]
+  :plugins [[lein-ring "0.9.3"]]
+  :ring {:handler ssbone.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
